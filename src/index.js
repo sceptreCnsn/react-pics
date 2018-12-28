e0153140dc8f850e57978f365fe6e46efcd0e57c
sceptreCnsn <cansin.memis@clb.lcl>
2018-12-28 11:04:58 +0300

    third

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
-//asdasdasdasdasd
+//asdasdasdasdasdasdasdasdasd
 ReactDOM.render(<App />, document.querySelector("#root"));
