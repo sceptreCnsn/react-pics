572a9780543ea93484526753edd528a2594a40f8
sceptreCnsn <cansin.memis@clb.lcl>
2018-12-28 11:05:18 +0300

    Fourth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
-//asdasdasdasdasdasdasdasdasd
+//asdasdasdasdasdasdasdasdasd cansin
 ReactDOM.render(<App />, document.querySelector("#root"));
