8eefe4b1c057943f7ff2027f4507424837f6442f
sceptreCnsn <cansin.memis@clb.lcl>
2018-12-28 18:29:30 +0300

    Grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,17 +1,28 @@
-import '../components/ImageList.css';
+import "../components/ImageList.css";
 import React from "react";
 
 class ImageList extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={x:null};
+    this.state = { x: null };
   }
 
   render() {
-    const list = this.props.images.map(({id,description,urls}) => {
-      return <img key={id} src={urls.regular} alt={description} style={{width:'100%'}}></img>
+    const list = this.props.images.map(({ id, description, urls }) => {
+      return (
+        <img
+          key={id}
+          src={urls.regular}
+          alt={description}
+          style={{ width: "100%" }}
+        />
+      );
     });
-    return <div className="image-list">{list}</div>;
+    return (
+      <div className="image-list-wrapper">
+        <div className="image-list">{list}</div>
+      </div>
+    );
   }
 }
 
