9e8ac22765704430889e04a09f9922a8ba620408
sceptreCnsn <cansin.memis@clb.lcl>
2018-12-28 11:04:12 +0300

    Second

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
-//asdasdasd
+//asdasdasdasdasd
 ReactDOM.render(<App />, document.querySelector("#root"));
